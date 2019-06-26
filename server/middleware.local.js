b6e486774af7b2a029f8feb4fd677a0ee7a5fab2
vps <admin@bubblesonline.nl>
2019-06-26 13:43:10 +0100

    remove whitelist on origin because some browser dont send origin in header

--- server/middleware.local.js
+++ server/middleware.local.js
@@ -8,14 +8,7 @@ module.exports = {
   initial: {
     cors: {
       params: {
-        origin: function(origin, callback) {
-		console.log('the origin is', origin);
-          if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true);
-          } else {
-            callback(new Error("Not allowed by CORS"));
-          }
-        }
+        origin: true
       }
     }
   }
