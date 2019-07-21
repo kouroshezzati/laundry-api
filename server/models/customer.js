b4ef981b5ffb1c77251feeead5438693a3989d59
moslem ezzati <moslem.ezati@gmail.com>
2019-07-21 21:07:40 +0430

    add resetPasswordRequest event to customer model

--- server/models/customer.js
+++ server/models/customer.js
@@ -1,5 +1,13 @@
-'use strict';
+"use strict";
 
 module.exports = function(Customer) {
+  Customer.on("resetPasswordRequest", function(info) {
+    console.log(info.email); // the email of the requested user
+    console.log(info.accessToken.id); // the temp access token to allow password reset
 
+    // requires AccessToken.belongsTo(User)
+    info.accessToken.user(function(err, user) {
+      console.log(user); // the actual user
+    });
+  });
 };
