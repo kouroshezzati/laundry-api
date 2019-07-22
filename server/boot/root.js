eb5ef32461596a3a2623c1cb39d88e0b560a9a30
moslem ezzati <moslem.ezati@gmail.com>
2019-07-22 18:56:19 +0430

    add test route so that testing email

--- server/boot/root.js
+++ server/boot/root.js
@@ -5,11 +5,34 @@
 
 "use strict";
 const ModelPagination = require("../../components/pagination/index");
+var nodemailer = require("nodemailer");
 
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
   router.get("/", server.loopback.status());
   router.get("/api/MyOrders/:id", ModelPagination);
+  router.get("/test", (req, res) => {
+    var transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "moslem.ezati@gmail.com",
+        pass: "Moslem_85_gmail"
+      }
+    });
+    var mailOptions = {
+      from: "moslem.ezati@gmail.com",
+      to: "moslem.ezati@gmail.com",
+      subject: "Sending Email using Node.js",
+      text: "That was easy!"
+    };
+    transporter.sendMail(mailOptions, function(error, info) {
+      if (error) {
+        return res.send(error);
+      } else {
+        return res.send("Email sent: " + info.response);
+      }
+    });
+  });
   server.use(router);
 };
