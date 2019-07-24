701ab137a102c8518f3461a77721e32475fcce6b
moslem ezzati <moslem.ezati@gmail.com>
2019-07-24 18:05:20 +0430

    add send email procedure to resetPasswordReqeust event

--- server/models/customer.js
+++ server/models/customer.js
@@ -1,13 +1,32 @@
 "use strict";
+const sendmail = require("sendmail")({
+  logger: {
+    debug: console.log,
+    info: console.info,
+    warn: console.warn,
+    error: console.error
+  },
+  smtpPort: 25, // Default: 25
+  smtpHost: "localhost" // Default: -1 - extra smtp host after resolveMX
+});
 
 module.exports = function(Customer) {
   Customer.on("resetPasswordRequest", function(info) {
     console.log(info.email); // the email of the requested user
     console.log(info.accessToken.id); // the temp access token to allow password reset
-
-    // requires AccessToken.belongsTo(User)
-    info.accessToken.user(function(err, user) {
-      console.log(user); // the actual user
-    });
+    sendmail(
+      {
+        from: "no-reply@bubblesonline.nl",
+        to: info.email,
+        subject: "reset password",
+        html: `<div>Your reset link is: <a href="https://www.bubblesonline.nl/reset-password/?${
+          info.accessToken.id
+        }">here.</a></div>`
+      },
+      function(err, reply) {
+        console.log(err && err.stack);
+        console.dir(reply);
+      }
+    );
   });
 };
